aa754b4b138d3bcc947a6febb66f69d028438512
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2026-03-24 15:43:48 +0530

    fix: removed /api from skill controller

--- backend/src/main/java/com/qubid/backend/controller/SkillController.java
+++ backend/src/main/java/com/qubid/backend/controller/SkillController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/skill")
+@RequestMapping("/skill")
 @RequiredArgsConstructor
 public class SkillController {
 
